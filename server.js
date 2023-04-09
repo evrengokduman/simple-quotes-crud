48f208408493a9c6c63559d1d2c4e2264cfdf28f
evren <sukruevrengokduman@gmail.com>
2023-04-09 15:28:05 +0300

    mongoose connection is done

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ require("dotenv").config();
 //add model variable
 
 //set middleware to use ejs
-app.set("viewengine", "ejs");
-app.use(express.static(public));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 //this helps validate passing right data back and forth
-app.use(express.urlencoded({ extenden: true })); //if we set this to false it wouldn't let us send a json object with an array
+app.use(express.urlencoded({ extended: true })); //if we set this to false it wouldn't let us send a json object with an array
 
-app.listen("/", (req, res) => {});
+//URI is the connection string
+
+// I did lots of things here. Mongoose callback here was deprecated so, I got the promise coming from db and the only way I could log was connect - then - catch
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to dbase");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
